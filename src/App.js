745cda461d47c20ad6e2f37dbb97a1816987f9ba
micaela-12 <micaelarobertob@gmail.com>
2022-07-05 20:22:57 -0300

    }

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Showclass Producciones!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="header">
+        <h1 className="header__TitleIndex">Showclass Producciones!</h1>
+        <nav className="header__contenedor">
+          <ul>
+          <li><button type="button" className= "header__color">Inicio</button></li>
+          <li><button type="button" className= "header__color">15 años</button></li>
+          <li><button type="button" className= "header__color">Casamiento</button></li>
+          <li><button type="button" className= "header__color">Cooporativo</button></li>
+          <li><button type="button" className= "header__color">Cumpleaños</button></li>
+          </ul>
+        </nav>
       </header>
     </div>
   );
 }
+
 
 export default App;
